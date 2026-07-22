31b1f39e3da3bd51c3045fc832e77ff7aac223f5
Alex Balyanov <alex44298@yandex.ru>
2026-07-22 16:29:10 +0500

    настроил prettier и eslint
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml
    #	src/app/page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="ru"
-    >
+    <html lang="ru">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HomePage from "@/src/components/pages/homePage";
 export default function Home() {
   return (
     <div>
+      <main></main>
       <main>
          <HomePage />
       </main>
