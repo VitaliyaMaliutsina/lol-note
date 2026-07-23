4bbd51d8ef4dc01968a0996d9212cb85a500a188
Alex Balyanov <alex44298@yandex.ru>
2026-07-23 20:38:22 +0500

    сделал компонент LinkButton

--- src/components/pages/homePage/index.tsx
+++ src/components/pages/homePage/index.tsx
@@ -1,10 +1,15 @@
 import styles from "./styles.module.scss";
 import Button from "@/components/Button";
+import LinkButton from "@/components/LinkButton";
 const HomePage = () => {
   return (
     <>
       <div className={styles.container}></div>
       <Button variant="primary">редактировать профиль</Button>
+      <LinkButton variant="outline" href="/login">
+        <span>{">"}</span>
+        показать все
+      </LinkButton>
     </>
   );
 };
