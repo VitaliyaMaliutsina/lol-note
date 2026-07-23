5de78647614d98c4aa737946289061fc038cdf1f
Alex Balyanov <alex44298@yandex.ru>
2026-07-23 19:46:02 +0500

    сделал компонент кнопки

--- src/components/pages/homePage/index.tsx
+++ src/components/pages/homePage/index.tsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.scss";
+import Button from "@/components/Button";
 const HomePage = () => {
-    return (
-        <div className={styles.container}>
-            Главная
-        </div>
-    );
+  return (
+    <>
+      <div className={styles.container}></div>
+      <Button variant="primary">Редактировать профиль</Button>
+    </>
+  );
 };
 
 export default HomePage;
