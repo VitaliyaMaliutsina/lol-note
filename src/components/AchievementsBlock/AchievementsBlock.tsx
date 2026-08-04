2355e3369d9a3fffc783c043f2ce5a3adeab3249
Vita <v.maliutsina@agima.ru>
2026-08-04 19:45:29 +0300

    lol-note-21 | cоздание страницы 404 и кастомного скроллбара lol-note-43

--- src/components/AchievementsBlock/AchievementsBlock.tsx
+++ src/components/AchievementsBlock/AchievementsBlock.tsx
@@ -2,6 +2,7 @@ import styles from "./AchievementsBlock.module.scss";
 import Image from "next/image";
 import achievementPlaceholder from "../../../public/images/locked.png";
 import { achievements } from "./data";
+import { clsx } from "clsx";
 
 const AchievementsBlock = () => {
   return (
@@ -26,7 +27,7 @@ const AchievementsBlock = () => {
         </button>
       </div>
 
-      <div className={styles.achievementWrapper}>
+      <div className={clsx(styles.achievementWrapper, styles.scrollbar)}>
         {achievements.map((achiev) => {
           return (
             <div key={achiev.id} className={styles.achievement}>
--- src/components/ProfileBlock/ProfileBlock.tsx
+++ src/components/ProfileBlock/ProfileBlock.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import React from "react";
 import Stats from "@/components/Stats";
 import Button from "@/components/ui/Button";
+import { clsx } from "clsx";
 
 const ProfileBlock = () => {
   const editProfile = () => {
@@ -53,7 +54,7 @@ const ProfileBlock = () => {
 
       <div className={styles.infoWrapper}>
         <h1 className={styles.titleName}>Катюха Гашишева</h1>
-        <p className={styles.description}>
+        <p className={clsx(styles.description, styles.scrollbar)}>
           Люблю анализировать матчапы и побеждать. Создаю заметки по всем линиям
           и делюсь опытом. Lorem ipsum dolor sit amet, consectetur adipisicing
           elit. Alias aliquid animi aperiam aspernatur cumque deleniti dolorem
